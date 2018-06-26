a241c80b75fa08bf57762ebc2537e6d544dc380f
ireniero <ireniero@hexacta.com>
2018-06-26 13:55:09 -0300

    Added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomAngularMaterialModule } from './custom-angular-material/custom-angular-material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CustomAngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
